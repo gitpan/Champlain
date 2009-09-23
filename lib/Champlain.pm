e67430205ec133e72a11718ac2f658d8d58f9101
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2009-09-23 17:21:18 +0000

    Import of POTYL/Champlain-0.08 from CPAN.
    gitpan-cpan-distribution: Champlain
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         POTYL/Champlain-0.08.tar.gz
    gitpan-cpan-author:       POTYL
    gitpan-cpan-maturity:     released

--- lib/Champlain.pm
+++ lib/Champlain.pm
@@ -15,9 +15,8 @@ Champlain - Map rendering canvas
 	
 	# Create the map view and set some properties
 	my $map = Champlain::View->new();
-	$map->set('scroll-mode', 'kinetic');
-	$map->set_size(800, 600);
-	$map->set_property('zoom-level', 7);
+	$map->set_size($stage->get_size);
+	$map->set_zoom_level(7);
 	$map->center_on(45.466, -73.75);
 	
 	# Pack the actors	
@@ -30,11 +29,8 @@ Champlain - Map rendering canvas
 =head1 DESCRIPTION
 
 Champlain is a Perl binding for the C library libchamplain which provides a
-canvas widget based on Clutter that displays maps from various free map sources
-such as I<OpenStreetMap>, I<OpenAerialMap> and I<Maps for free>.
-
-If the C library is compiled with GTK support then the map widget can also be
-embedded in any GTK application.
+canvas widget based on L<Clutter> that displays maps from various free map
+sources such as I<OpenStreetMap>, I<OpenAerialMap> and I<Maps for free>.
 
 For more information about libchamplain see:
 L<http://projects.gnome.org/libchamplain/>.
@@ -59,6 +55,21 @@ The tag I<coords> can be used for importing the constants providing the minimal
 and maximal values for (latitude, longitude) coordinates:
 
 	use Champlain ':coords';
+
+=head1 Gtk2 support
+
+In the past I<Champlain> provided also support for a L<Gtk2> widget if the C
+library libchamplain was compiled with GTK support and if Clutter would be
+built with Gtk2 support.
+
+While this was very handy it made the Perl modules hard to maintain and starting
+with Clutter 1.0 the Gtk2 wrappers are no longer bundled. They will be provided
+by another CPAN package that doesn't exist yet. This will mimic what the C
+libraries do and what other bindings do.
+
+In the meanwhile this means that this Perl module no longer supports Gtk2.
+Just as with Clutter, another module will be created that will add support for
+Gtk2 once the Gkt2 Perl bindings for Clutter are written.
 
 =head1 BUGS
 
@@ -108,7 +119,7 @@ For the terms of The Artistic License, see L<perlartistic>.
 use warnings;
 use strict;
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 use base 'DynaLoader';
 use Exporter 'import';
